560ce31ab27a196f372705924f4d4774d717c4ed
yusiqo <80514373+yusiqo@users.noreply.github.com>
2022-07-10 21:05:52 +0400

    Create kur.sh

---
+++ kur.sh
@@ -0,0 +1,6 @@
+sudo apt update -y 
+sudo apt install xfce4 xfce4-goodies xrdp -y
+sudo apt install -y tightvncserver -y
+sudo apt install firefox-esr -y
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb

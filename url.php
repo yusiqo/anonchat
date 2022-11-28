bdbdaf7e68d687c95d04418e8249b86f9660f18d
yusiqo <80514373+yusiqo@users.noreply.github.com>
2022-11-28 08:14:10 +0400

    Create url.php

---
+++ url.php
@@ -0,0 +1,6 @@
+<?php 
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+ 
+$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+echo $url; // 
+?>

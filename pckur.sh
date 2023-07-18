23ec9d998cfd453c16befcc1403b0e54a3523c87
yusiqo <80514373+yusiqo@users.noreply.github.com>
2023-07-18 23:20:16 +0400

    Create pckur.sh

---
+++ pckur.sh
@@ -0,0 +1,9 @@
+clear
+echo "Ghost Tekno Özel Scriptidi. pc qurulur"
+sleep 5
+sudo apt install xfce4 xfce4-goodies xrdp -y 
+sudo apt install -y tightvncserver -y
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+clear 
+echo "Kurulum Eledim. GhostTekno Salamlayır, yusiqo dan kodu al"
